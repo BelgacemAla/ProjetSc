9e483b701afdd4ac2d2aa440ae056f8215623caf
ala belgacem <alabelgacem27f@gmail.com>
2026-03-07 22:14:06 +0100

    ajout de fonction d'application d'algo glouton sur les maps

--- src/main.jl
+++ src/main.jl
@@ -2,7 +2,10 @@ include("carte.jl")
 include("BFS.jl")
 include("Aetoile.jl")
 include("dijkstra.jl")
+include("glouton.jl")
 include("lecture_map.jl")
+
+# Application directe des algorithmes sur les maps dans dossier test 
 
 function algoBFS(fname, D::Tuple{Int64,Int64}, A::Tuple{Int64,Int64})
     c = map_to_carte(fname, Position(D[1],D[2]), Position(A[1],A[2]))
@@ -33,6 +36,19 @@ end
 function algoAstar(fname::String, D::Tuple{Int64,Int64}, A::Tuple{Int64,Int64})
     c = map_to_carte(fname, Position(D[1],D[2]), Position(A[1],A[2]))
     chemin, cout, noeuds = Astar(c)
+    if chemin === nothing
+        println("Aucun chemin trouvé")
+    else
+        println("Distance D → A : ", cout)
+        println("Nombre d'états explorés : ", noeuds)
+        print("Path D → A : ")
+        println(join(["($(p.x), $(p.y))" for p in chemin], "→"))
+    end
+end
+
+function algoGlouton(fname::String, D::Tuple{Int64,Int64}, A::Tuple{Int64,Int64})
+    c = map_to_carte(fname, Position(D[1],D[2]), Position(A[1],A[2]))
+    chemin, cout, noeuds = glouton(c)
     if chemin === nothing
         println("Aucun chemin trouvé")
     else
