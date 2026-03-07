8ca4f7aaa2e061a53fb33b106a6cf5de478a515a
ala belgacem <alabelgacem27f@gmail.com>
2026-03-07 21:51:44 +0100

    ajout des fonction d'application des algorithmes sur les maps

--- src/main.jl
+++ src/main.jl
@@ -2,30 +2,43 @@ include("carte.jl")
 include("BFS.jl")
 include("Aetoile.jl")
 include("dijkstra.jl")
+include("lecture_map.jl")
 
-grille = [3  0  0  0 ;
-          -1 -1 -1 0;
-          0  5  0  0]
-c = creation_carte(grille, Position(3,1), Position(1,4))
-affichage_carte(c)
-
-function tester_algo(algo, carte, nom_algo)
-    t_start = time()
-    chemin, cout, noeuds = algo(carte)
-    t_end = time()
-    temps = t_end - t_start
-
+function algoBFS(fname, D::Tuple{Int64,Int64}, A::Tuple{Int64,Int64})
+    c = map_to_carte(fname, Position(D[1],D[2]), Position(A[1],A[2]))
+    chemin, cout, noeuds = bfs(c)
     if chemin === nothing
-        println("$nom_algo : Aucun chemin trouvé")
+        println("Aucun chemin trouvé")
     else
-        println("$nom_algo :")
-        println("  Longueur chemin : ", length(chemin)-1)
-        println("  Coût total      : ", cout)
-        println("  Noeuds explorés : ", noeuds)
-        println("  Temps           : ", temps)
+        println("Distance D → A : ", cout)
+        println("Nombre d'états explorés : ", noeuds)
+        print("Path D → A : ")
+        println(join(["($(p.x), $(p.y))" for p in chemin], "→"))
     end
 end
 
-tester_algo(bfs, c, "BFS")
-tester_algo(Aetoile, c, "A*")
-tester_algo(dijkstra, c, "Dijkstra")  
+function algoDijkstra(fname::String, D::Tuple{Int64,Int64}, A::Tuple{Int64,Int64})
+    c = map_to_carte(fname, Position(D[1],D[2]), Position(A[1],A[2]))
+    chemin, cout, noeuds = dijkstra(c)
+    if chemin === nothing
+        println("Aucun chemin trouvé")
+    else
+        println("Distance D → A : ", cout)
+        println("Nombre d'états explorés : ", noeuds)
+        print("Path D → A : ")
+        println(join(["($(p.x), $(p.y))" for p in chemin], "→"))
+    end
+end
+
+function algoAstar(fname::String, D::Tuple{Int64,Int64}, A::Tuple{Int64,Int64})
+    c = map_to_carte(fname, Position(D[1],D[2]), Position(A[1],A[2]))
+    chemin, cout, noeuds = Astar(c)
+    if chemin === nothing
+        println("Aucun chemin trouvé")
+    else
+        println("Distance D → A : ", cout)
+        println("Nombre d'états explorés : ", noeuds)
+        print("Path D → A : ")
+        println(join(["($(p.x), $(p.y))" for p in chemin], "→"))
+    end
+end
